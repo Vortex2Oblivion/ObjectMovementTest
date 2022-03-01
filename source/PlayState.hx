b17e1a8537a94893462821767fb424c966480892
bob <bob@>
2022-03-01 08:36:30 -0500

    Images Now Pog

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,20 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var sprite:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
+		sprite = new FlxSprite();
+		sprite.loadGraphic(AssetPaths.wp__png);
+		sprite.x = 100;
+		sprite.y = -50;
+		add(sprite);
 	}
 
 	override public function update(elapsed:Float)
