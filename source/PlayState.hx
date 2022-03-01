3f426d30fca802325f3e6b70692018338b1b1851
bob <bob@>
2022-03-01 14:31:30 -0500

    Finally the thing does the funni. Thanks, Leather.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.tweens.FlxTween;
+import js.html.svg.Angle;
 
 class PlayState extends FlxState
 {
@@ -12,6 +13,7 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+
 		sprite = new FlxSprite();
 		sprite.loadGraphic(AssetPaths.wp__png);
 		sprite.x = 100;
@@ -19,10 +21,10 @@ class PlayState extends FlxState
 		add(sprite);
 
 		FlxTween.tween(sprite, {
-			x: FlxG.width - sprite.width
-			y: FlxG.height - sprite.height
-			angle: 360.0
-		}, 5, {type: FlxTween.PINGPONG});
+			x: FlxG.width - sprite.width,
+			y: FlxG.height - sprite.height,
+			angle: 360
+		}, 5, {type: FlxTweenType.PINGPONG});
 	}
 
 	override public function update(elapsed:Float)
