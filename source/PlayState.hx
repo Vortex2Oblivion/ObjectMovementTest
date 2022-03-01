ff5a02889727f97af8d4dfba2b66c53fcf029ab9
bob <bob@>
2022-03-01 08:51:42 -0500

    Compile Error i will fix later lol just pushing this so i dont loose it.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,7 +1,9 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.tweens.FlxTween;
 
 class PlayState extends FlxState
 {
@@ -15,6 +17,12 @@ class PlayState extends FlxState
 		sprite.x = 100;
 		sprite.y = -50;
 		add(sprite);
+
+		FlxTween.tween(sprite, {
+			x: FlxG.width - sprite.width
+			y: FlxG.height - sprite.height
+			angle: 360.0
+		}, 5, {type: FlxTween.PINGPONG});
 	}
 
 	override public function update(elapsed:Float)
