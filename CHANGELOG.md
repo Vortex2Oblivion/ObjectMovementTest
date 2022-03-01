1e49a502fdd114ed41da734cd8f517aca06f9f6d
bob <bob@>
2022-03-01 15:25:49 -0500

    Thanks, Leather128.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-0.0.0 --- 1/3/2022
-
+0.0.3 --- 3/1/2022
+Pixel Perfect Rendering!
+0.0.2 --- 3/1/2022
+Image moves now in cool ways. Thanks, Leather128.
+0.0.1 --- 3/1/2022
+Added images, the whole point of thie thing.
+0.0.0 --- 3/1/2022S
 Nothing This is just a base haxeflixel project lol.
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,69 +3,34 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import js.html.svg.Angle;
 
 class PlayState extends FlxState
 {
-	var text:FlxText;
 	var sprite:FlxSprite;
-	var grid:FlxSprite;
 
 	override public function create()
 	{
 		super.create();
 
-		// I looked up image on google and this was the first result.
-		// Makes a sprite at X: 100, Y: 0
-		sprite = new FlxSprite(100, 0);
-		sprite.loadGraphic(AssetPaths.Image__png);
-
-		// Makes a sprite at X: 100, Y: 0
-		grid = new FlxSprite(100, 0);
-		grid.makeGraphic(300, 300, flixel.util.FlxColor.WHITE);
-		// Pixel Perfect Rendering so the grid doesn't look wonky.
-		grid.pixelPerfectRender = true;
-
-		// Set the grid pixels.
-		for (y in 0...Std.int(grid.width))
-		{
-			for (x in 0...Std.int(grid.height))
-			{
-				if (x % 2 == 1 && y % 2 == 1)
-					grid.pixels.setPixel(x, y, 0x0000ff);
-				if (x < 5 || y < 5 || x > 295 || y > 295)
-					grid.pixels.setPixel(x, y, 0xffffff);
-			}
-		}
-
-		// Creates text saying "Hello World" in the middle of the screen in red text.
-		text = new FlxText(0, 0, FlxG.width, "Hello World", 64);
-		text.setFormat(null, 64, FlxColor.RED, FlxTextAlign.CENTER);
-
-		add(text);
+		sprite = new FlxSprite();
+		sprite.loadGraphic(AssetPaths.wp__png);
+		// Pixel Perfect Rendering so the sprite doesn't look wonky.
+		sprite.pixelPerfectRender = true;
+		sprite.x = 100;
+		sprite.y = -50;
 		add(sprite);
-		add(grid);
+
+		FlxTween.tween(sprite, {
+			x: FlxG.width - sprite.width,
+			y: FlxG.height - sprite.height,
+			angle: 360
+		}, 5, {type: FlxTweenType.PINGPONG});
 	}
 
-	override public function update(elapsed:Float):Void // Moves the text along the y axis to the bottom of the screen.
+	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
-
-		// Moves the text 1 pixel down.
-		text.y++;
-		// Moves the sprite and grid 1 pixel down.
-		sprite.x++;
-		grid.x++;
-
-		// Brings things back to screen if they are offscreen.
-		if (text.y > FlxG.height)
-			text.y = 0 - 64;
-
-		if (sprite.x > FlxG.height)
-			sprite.x = 0 - 64;
-
-		if (grid.x > FlxG.height)
-			grid.x = 0 - 64;
 	}
 }
